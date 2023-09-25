0b8a84794ebb82d0379920f9c8f126d343c34007
111 <222>
2023-09-25 17:02:01 +0800

    1

---
+++ CrossRVMSafe/CrossRVMSafe/CrossRVMSafe.cpp
@@ -0,0 +1,17 @@
+﻿#include "SilentJack2-Client.hpp"
+#include <iostream>
+
+int main()
+{
+	SilentJack sj;
+	sj.Init();
+	sj.GetHandle(L"Game.exe");
+
+	DWORD64 world = 0;
+	sj.qRVM(0x1000, &world, sizeof(DWORD64));
+
+	cout << "read @ 0x" << hex << world << endl;
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
---
+++ CrossRVMSafe/readme.md
@@ -0,0 +1,3 @@
+https://www.unknowncheats.me/forum/anti-cheat-bypass/261176-silentjack-ultimate-handle-hijacking-user-mode-multi-ac-bypass-eac-tested.html
+
+code run in win7
